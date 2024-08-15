64367d9bd3f7d2fa567a64e19aa97139fb32a5ad
Advait Patel <beinspired118@gmail.com>
2024-08-15 20:17:21 +0000

    added rolling norm - getting nans

--- environment.py
+++ environment.py
@@ -19,9 +19,24 @@ from brax.envs.base import PipelineEnv, State
 from brax.io import mjcf
 from brax.mjx.base import State as MjxState
 
+RUNNING_LENGTH = 10
+EPSILON = 1e-4
 
 
 logger = logging.getLogger(__name__)
+
+REWARD_CONFIG = {
+    "termination_height": 0,
+    "height_limits": {"min_z": 0, "max_z": 2.0},
+    "is_healthy_reward": 5,
+    "original_pos_reward": {
+        "exp_coefficient": -2,
+        "subtraction_factor": 0.2,
+        "max_diff_norm": 0.5,
+    },
+    "ctrl_cost_coefficient": 0.1,
+    "weights": {"ctrl_cost": 0.1, "original_pos_reward": 1, "is_healthy": 5},
+}
 
 
 def download_model_files(repo_url: str, repo_dir: str, local_path: str) -> None:
@@ -84,7 +99,7 @@ class HumanoidEnv(PipelineEnv):
     _action_size: int
     reset_noise_scale: float = 0.0
 
-    def __init__(self, n_frames: int = 1, backend: str = "mjx") -> None:
+    def __init__(self, n_frames: int = 1, backend: str = "mjx", gamma = 0.99) -> None:
         """Initializes system with initial joint positions, action size, the model, and update rate."""
         # GitHub repository URL
         repo_url = "https://github.com/nathanjzhao/mujoco-models.git"
@@ -110,6 +125,8 @@ class HumanoidEnv(PipelineEnv):
         self._action_size = mj_model.nu
         sys: base.System = mjcf.load_model(mj_model)
 
+        self.gamma = gamma
+
         super().__init__(sys, n_frames=n_frames, backend=backend)
 
     @partial(jax.jit, static_argnums=(0,))
@@ -134,6 +151,14 @@ class HumanoidEnv(PipelineEnv):
             "returned_episode_lengths": 0,
             "timestep": 0,
             "returned_episode": False,
+            "return_avg": 0,
+            "obs_avg": 0,
+            "return_var": 1,
+            "obs_var": 1,
+            "return_norm": 0,
+            "obs_norm": 0,
+            "return_count": EPSILON,
+            "obs_count": EPSILON
         }
 
         return State(state, obs, jnp.array(0.0), False, metrics)
@@ -173,9 +198,51 @@ class HumanoidEnv(PipelineEnv):
         # Get previous metrics
         metrics = env_state.metrics
 
+        state_obs_avg = metrics["obs_avg"] * (1 - done)
+        state_obs_var = metrics["obs_var"] * (1 - done) + done * 1
+        state_obs_count = metrics["obs_count"]*(1 - done) + done * EPSILON
+        obs_delta = jnp.mean(obs) - state_obs_avg
+        tot_obs_count = state_obs_count + 1
+        new_obs_mean = state_obs_avg + obs_delta*tot_obs_count
+
+        obs_m_a = (state_obs_var)*(state_obs_count)
+        #m_b = 0 since var of 1 item is 0
+        obs_M2 = obs_m_a + jnp.square(obs_delta) * state_obs_count / (tot_obs_count + EPSILON)
+        new_obs_var = obs_M2 / tot_obs_count
+        new_obs_count = tot_obs_count
+
+        metrics["obs_avg"] = new_obs_mean * (1 - done)
+        metrics["obs_var"] = new_obs_var * (1 - done) + done * 1
+        metrics["obs_count"] = new_obs_count * (1 - done) + done * 1
+
+        metrics["obs_norm"] = (jnp.mean(obs) - metrics["obs_avg"]) * (1 - done) / (jnp.sqrt(metrics["obs_var"] + EPSILON))
+
+        state_return_val = metrics["episode_returns"] * (1 - done)
+        state_return_avg = metrics["return_avg"] * (1 - done)
+        state_return_var = metrics["return_var"] * (1 - done)  + done * 1
+        state_return_count = metrics["return_count"] * (1 - done) + done * EPSILON
+        return_val = state_return_val * self.gamma * (1 - done) + reward
+        return_delta = return_val - state_return_avg
+        tot_return_count = state_return_count + 1
+        new_return_mean = state_return_avg + return_delta / (tot_return_count + EPSILON)
+        return_m_a = state_return_var * state_return_count
+        return_M2 = return_m_a + jnp.square(return_delta) * state_return_count / (tot_return_count + EPSILON)
+        new_return_var = return_M2 / (EPSILON + tot_return_count)
+        new_return_count = tot_return_count
+
+        metrics["return_avg"] = new_return_mean * (1 - done)
+        metrics["return_var"] = new_return_var * (1 - done) + 1 * done
+        metrics["return_count"] = new_return_count * (1 - done)
+
+        metrics["return_norm"] = (reward * (1 - done)) / (jnp.sqrt(metrics["return_var"] + 1e-8))
+
+
+
+
         # Calculate new episode return and length
         new_episode_return = metrics["episode_returns"] + reward
         new_episode_length = metrics["episode_lengths"] + 1
+
 
         # Update metrics -- we only count episode
         metrics["episode_returns"] = new_episode_return * (1 - done)
@@ -190,7 +257,7 @@ class HumanoidEnv(PipelineEnv):
         metrics["returned_episode"] = done
 
         return env_state.replace(
-            pipeline_state=state, obs=obs, reward=reward, done=done, metrics=metrics
+            pipeline_state=state, obs=jnp.ones_like(obs)*metrics["obs_norm"], reward=metrics["return_norm"], done=done, metrics=metrics
         )
 
     @partial(jax.jit, static_argnums=(0,))
@@ -198,30 +265,36 @@ class HumanoidEnv(PipelineEnv):
         self, state: MjxState, next_state: MjxState, action: jnp.ndarray
     ) -> jnp.ndarray:
         """Compute the reward for standing and height."""
-        min_z, max_z = 0, 2.0
-        # min_z, max_z = -0.35, 2.0
+        min_z, max_z = (
+            REWARD_CONFIG["height_limits"]["min_z"],
+            REWARD_CONFIG["height_limits"]["max_z"],
+        )
+
+        exp_coef, subtraction_factor, max_diff_norm = (
+            REWARD_CONFIG["original_pos_reward"]["exp_coefficient"],
+            REWARD_CONFIG["original_pos_reward"]["subtraction_factor"],
+            REWARD_CONFIG["original_pos_reward"]["max_diff_norm"],
+        )
+
         is_healthy = jnp.where(state.q[2] < min_z, 0.0, 1.0)
         is_healthy = jnp.where(state.q[2] > max_z, 0.0, is_healthy)
 
-        diff_cost = -jnp.sum(jnp.square(self.sys.qpos0 - state.qpos))
+        diff = self.sys.qpos0 - state.qpos
+        original_pos_reward = jnp.exp(
+            -exp_coef * jnp.linalg.norm(diff)
+        ) - subtraction_factor * jnp.clip(jnp.linalg.norm(diff), 0, max_diff_norm)
+
         ctrl_cost = -jnp.sum(jnp.square(action))
 
         # xpos = state.subtree_com[1][0]
         # next_xpos = next_state.subtree_com[1][0]
         # velocity = (next_xpos - xpos) / self.dt
 
-        # jax.debug.print(
-        #     "velocity {}, xpos {}, next_xpos {}",
-        #     velocity,
-        #     xpos,
-        #     next_xpos,
-        #     ordered=True,
-        # )
-        # jax.debug.print("ctrl_cost {}, is_healthy {}, height {}", ctrl_cost, is_healthy, state.q[2], ordered=True)
-
-        # jax.debug.print("diff {}", diff_cost)
-
-        total_reward = 0.1 * ctrl_cost + 0.1 * diff_cost + 5 * is_healthy
+        total_reward = (
+            REWARD_CONFIG["weights"]["ctrl_cost"] * ctrl_cost
+            + REWARD_CONFIG["weights"]["original_pos_reward"] * original_pos_reward
+            + REWARD_CONFIG["weights"]["is_healthy"] * is_healthy
+        )
         # total_reward = 0.1 * ctrl_cost + 5 * is_healthy + 1.25 * velocity
 
         return total_reward
@@ -233,13 +306,8 @@ class HumanoidEnv(PipelineEnv):
         com_height = state.q[2]
 
         # Set a termination threshold
-        termination_height = 0
-        # termination_height = -0.35
+        termination_height = REWARD_CONFIG["termination_height"]
 
-        # Episode is done if the robot falls below the termination height
-        # done = jnp.where(com_height < termination_height, 1.0, 0.0)
-
-        # return com_height < termination_height
         return com_height < termination_height
 
     @partial(jax.jit, static_argnums=(0,))
@@ -251,11 +319,12 @@ class HumanoidEnv(PipelineEnv):
             data.cvel[1:].ravel(),
             data.qfrc_actuator,
         ]
-
+        
         return jnp.concatenate(obs_components)
 
 
 ################## WRAPPERS ##################
+
 
 class EnvWrapper(object):
     """Base class for Gymnax wrappers."""
@@ -266,6 +335,7 @@ class EnvWrapper(object):
     # provide proxy access to regular attributes of wrapped object
     def __getattr__(self, name):
         return getattr(self._env, name)
+
 
 # @struct.dataclass
 # class LogEnvState:
@@ -319,6 +389,7 @@ class EnvWrapper(object):
 #         info["returned_episode"] = done
 #         return obs, state, reward, done, info
 
+
 class ClipAction(EnvWrapper):
     def __init__(self, env, low=-1.0, high=1.0):
         super().__init__(env)
@@ -333,7 +404,7 @@ class ClipAction(EnvWrapper):
             obs=env_state.obs,
             reward=env_state.reward,
             done=env_state.done,
-            metrics=env_state.metrics
+            metrics=env_state.metrics,
         )
 
 
@@ -349,7 +420,7 @@ class TransformObservation(EnvWrapper):
             obs=self.transform_obs(env_state.obs),
             reward=env_state.reward,
             done=env_state.done,
-            metrics=env_state.metrics
+            metrics=env_state.metrics,
         )
 
     def step(self, state, action, key):
@@ -359,7 +430,7 @@ class TransformObservation(EnvWrapper):
             obs=self.transform_obs(env_state.obs),
             reward=env_state.reward,
             done=env_state.done,
-            metrics=env_state.metrics
+            metrics=env_state.metrics,
         )
 
 
@@ -375,7 +446,7 @@ class TransformReward(EnvWrapper):
             obs=env_state.obs,
             reward=self.transform_reward(env_state.reward),
             done=env_state.done,
-            metrics=env_state.metrics
+            metrics=env_state.metrics,
         )
 
 
@@ -383,13 +454,14 @@ class VecEnv(EnvWrapper):
     def __init__(self, env):
         super().__init__(env)
         self.reset = jax.vmap(self._env.reset, in_axes=(0))
-        self.step = jax.vmap(self._env.step, in_axes=(None, 0, 0)) # state
+        self.step = jax.vmap(self._env.step, in_axes=(None, 0, 0))  # state
 
 
 class NormalizeVecObsState(State):
     norm_mean: jnp.ndarray
     norm_var: jnp.ndarray
     norm_count: float
+
 
 class NormalizeVecObservation(EnvWrapper):
     def __init__(self, env):
@@ -402,7 +474,7 @@ class NormalizeVecObservation(EnvWrapper):
             **state.__dict__,
             norm_mean=jnp.zeros_like(obs),
             norm_var=jnp.ones_like(obs),
-            norm_count=1e-4
+            norm_count=1e-4,
         )
 
     def step(self, state, action, key):
@@ -429,11 +501,13 @@ class NormalizeVecObservation(EnvWrapper):
             **next_state.__dict__,
             norm_mean=new_mean,
             norm_var=new_var,
-            norm_count=new_count
+            norm_count=new_count,
         )
 
         # Normalize the observation
-        new_state.obs = (obs - new_state.norm_mean) / jnp.sqrt(new_state.norm_var + 1e-8)
+        new_state.obs = (obs - new_state.norm_mean) / jnp.sqrt(
+            new_state.norm_var + 1e-8
+        )
 
         return new_state
 
@@ -470,12 +544,14 @@ class NormalizeVecReward(EnvWrapper):
             obs=obs,
             reward=env_state.reward,
             done=env_state.done,
-            metrics=env_state.metrics
+            metrics=env_state.metrics,
         )
 
     def step(self, state, action, key):
         env_state = self._env.step(state.pipeline_state, action, key)
-        return_val = state.return_val * self.gamma * (1 - env_state.done) + env_state.reward
+        return_val = (
+            state.return_val * self.gamma * (1 - env_state.done) + env_state.reward
+        )
 
         batch_mean = jnp.mean(return_val, axis=0)
         batch_var = jnp.var(return_val, axis=0)
@@ -504,8 +580,9 @@ class NormalizeVecReward(EnvWrapper):
             obs=env_state.obs,
             reward=normalized_reward,
             done=env_state.done,
-            metrics=env_state.metrics
+            metrics=env_state.metrics,
         )
+
 
 ################## TEST ENVIRONMENT RUN ##################
 
